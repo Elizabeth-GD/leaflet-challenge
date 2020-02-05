f3f9ea526c30ee963b8632eba8ee962364c16dce
Elizabeth-GD <54477823+Elizabeth-GD@users.noreply.github.com>
2020-02-05 00:51:03 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZWxpZ2QyMCIsImEiOiJjazUzNW1mM3IwNTR3M2VwbmxvZGNpaTJkIn0.m18zVIdR_jrg2NNB_6iO8w";
+
+const attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
